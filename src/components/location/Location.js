c05505ece7484b50c1ab7613f328b5af7a139232
Brant2001 <brant.pippin@gmail.com>
2020-04-20 15:19:07 -0500

    added the basic structure and some prliminary code

---
+++ src/components/location/Location.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels South</h3>
+        <div className="location__address">Somewhere Over The Rainbow</div>
+    </section>
+)
