ea4c121644383a894aa3f01e8638d0a297a5b6f7
Brant2001 <brant.pippin@gmail.com>
2020-04-22 10:14:09 -0500

    fixed an error and now have the animal list showing the owner and location tied to that specific animal

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -2,15 +2,26 @@ import React, { useContext } from 'react'
 import { AnimalContext } from './AnimalProvider'
 import Animal from "./Animal"
 import "./Animal.css"
+import { LocationContext } from '../location/LocationProvider'
+import { CustomerContext } from '../customer/CustomerProvider'
 
 
 export default () => {
     const { animals } = useContext(AnimalContext)
+    const { locations } = useContext(LocationContext)
+    const { customers } = useContext(CustomerContext)
 
     return (
         <div className="animals">
             {
-                animals.map(ani => <Animal key={ani.id} animal={ani} />)
+                animals.map(animal => {
+                    const matchingLocation = locations.find(loc => loc.id === animal.locationId)
+                    const matchingCustomer = customers.find(cus => cus.id === animal.customerId)
+                    
+                    return <Animal key={animal.id} animal={animal} 
+                        customer={matchingCustomer}
+                        location={matchingLocation}/>
+                })
             }
         </div>
     )
