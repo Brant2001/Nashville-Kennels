a3b6ee6df1de5671cd56345b57d5538c43337587
Brant2001 <brant.pippin@gmail.com>
2020-04-22 15:56:21 -0500

    copied necessary code from the chapters but still is not working

---
+++ src/components/Dashboard.js
@@ -0,0 +1,55 @@
+import React from "react"
+import "./animal/Animal.css"
+import "./customer/Customer.css"
+import "./employee/Employee.css"
+import "./location/Location.css"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import AnimalList from "./animal/AnimalList"
+import { LocationProvider } from "./location/LocationProvider"
+import LocationList from "./location/LocationList"
+import { CustomerProvider } from "./customer/CustomerProvider"
+import CustomerList from "./customer/CustomerList"
+import { EmployeeProvider } from "./employee/EmployeeProvider"
+import EmployeeList from "./employee/EmployeeList"
+
+export default () => (
+    <>
+        <h2>Nashville Kennels</h2>
+        <small>Loving care when you're not there.</small>
+
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Puppy Way</div>
+        </address>
+
+        <h2>Animals</h2>
+        <AnimalProvider>
+            <CustomerProvider>
+                <EmployeeProvider>
+                    <LocationProvider>
+                        <LocationList />
+                        <AnimalList />
+                        <CustomerList />
+                        <EmployeeList />
+                    </LocationProvider>
+                </EmployeeProvider>
+            </CustomerProvider>
+        </AnimalProvider>
+
+        <h2>Customers</h2>
+        <CustomerProvider>
+            <CustomerList />
+        </CustomerProvider>
+      
+        <EmployeeProvider>
+            <LocationProvider>
+                <EmployeeList />
+            </LocationProvider>
+        </EmployeeProvider>
+
+        <h2>Locations</h2>
+        <LocationProvider>
+            <LocationList />
+        </LocationProvider>
+    </>
+)
